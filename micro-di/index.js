1b5eda77540483c90b6b512bf7368784b11a56b3
Stefan Zerkalica <zerkalica@gmail.com>
2014-09-25 18:20:33 +0400

    updated to new micro di

--- micro-di/index.js
+++ micro-di/index.js
@@ -1,17 +1,13 @@
-var path         = require('path');
 var commonConfig = require('./common');
 var masterConfig = require('./master');
 var childConfig  = require('./child');
+var ul           = require('../lib/ul');
 
 module.exports = function (microDi, isChild) {
+	microDi.addModules({UltimateLogger: ul});
 	microDi.addVariables({
-		'ultimate-logger.libdir': path.dirname(require.resolve('../lib/logger')),
 		'log.dir': '%project.rootdir%/tests/logs'
 	});
 	microDi.addConfig(commonConfig);
-	if (isChild) {
-		microDi.addConfig(childConfig);
-	} else {
-		microDi.addConfig(masterConfig);
-	}
+	microDi.addConfig(isChild ? childConfig : masterConfig);
 };
